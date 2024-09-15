ae92e29eac66951b8cdf52a4dc6ffcbd1ec6ce99
Jason Lieu <jasontulieut@gmail.com>
2024-09-15 03:56:26 -0700

    time constants in control.py

--- server/control.py
+++ server/control.py
@@ -3,6 +3,8 @@ import time
 from ultrasonic import measure_distance, gpio_clean_up
 from directions import Directions
 
+WALK_TIME = 5
+TURN_TIME = 5
 STOP_DISTANCE = 50
 
 try:
@@ -14,13 +16,13 @@ try:
         if measure_distance() < STOP_DISTANCE:
             print(f"obstacle in {distance}cm, turn right")
             SpiderG.walk(Directions.TURN_RIGHT.value)
-            time.sleep(5)
+            time.sleep(TURN_TIME)
 
         # Else, walk forward
         else:
             print("no obstacle, forward")
             SpiderG.walk(Directions.FORWARD.value)
-            time.sleep(5)
+            time.sleep(WALK_TIME)
 except KeyboardInterrupt:
     print("clean up")
     gpio_clean_up()
