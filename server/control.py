7dd686422be6a376332764134edd47c589cf2f23
Jason Lieu <jasontulieut@gmail.com>
2024-09-15 17:55:07 -0700

    - turn left instead - reduce sensor distance

--- server/control.py
+++ server/control.py
@@ -5,17 +5,17 @@ from directions import Directions
 
 WALK_TIME = 5
 TURN_TIME = 5
-STOP_DISTANCE = 50
+STOP_DISTANCE = 25
 
 try:
     # Initial position
     SpiderG.move_init()
     while True:
-        # If object detected by ultrasonic sensor, turn right
+        # If object detected by ultrasonic sensor, turn left
         distance = measure_distance()
         if measure_distance() < STOP_DISTANCE:
-            print(f"obstacle in {distance}cm, turn right")
-            SpiderG.walk(Directions.TURN_RIGHT.value)
+            print(f"obstacle in {distance}cm, turn left")
+            SpiderG.walk(Directions.TURN_LEFT.value)
             time.sleep(TURN_TIME)
 
         # Else, walk forward
